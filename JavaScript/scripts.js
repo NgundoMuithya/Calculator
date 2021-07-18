ae6f2e63eea7519d387f2871a9dc1a5af92bc484
Ngundo Muithya <ngundolarrymuithya@gmail.com>
2021-07-18 16:10:38 +0300

    Link scripts.js to calculator.html and begin coding.

---
+++ JavaScript/scripts.js
@@ -0,0 +1,11 @@
+function add() {
+    var number1 = prompt("Enter a number:");
+    let number2 = prompt("Enter another number:");
+    return parseFloat(number1) + parseInt(number2);
+} 
+
+function subtract() {
+    var number1 = prompt("Enter a number:");
+    var number2 = prompt("Enter another number:");
+    return parseFloat(number1) - parseFloat(number2);  
+}
